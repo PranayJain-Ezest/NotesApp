34f4c4d3c5c32ef02ebaca252ef70549ae25e2ba
Pranay jain <pranay.jain@e-zest.in>
2023-03-30 15:24:49 +0530

    ui changes

--- app/src/main/java/com/example/notesapp/RegisterFragment.kt
+++ app/src/main/java/com/example/notesapp/RegisterFragment.kt
@@ -5,27 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.notesapp.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
 
-
+private var _binding:FragmentRegisterBinding?= null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding= FragmentRegisterBinding.inflate(inflater,container,false)
+
+
+
+
+
+       binding.txtRedirect.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+        return binding.root
+
+
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
 
 }
