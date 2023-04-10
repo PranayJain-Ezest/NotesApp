b81647ae4b16d63c90e9c0c6a72966a1eec2d0e1
Pranay.Jain <pranay.jain@e-zest.in>
2023-04-10 13:48:06 +0530

    updated click listner on LoginFragment.kt

--- app/src/main/java/com/example/notesapp/LoginFragment.kt
+++ app/src/main/java/com/example/notesapp/LoginFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.notesapp.databinding.FragmentLoginBinding
+import com.example.notesapp.databinding.FragmentRegisterBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private var _binding: FragmentLoginBinding?= null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding= FragmentLoginBinding.inflate(inflater,container,false)
+
+
+        _binding?.btnLogin?.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+
+
+        return binding.root
+    }
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 }
